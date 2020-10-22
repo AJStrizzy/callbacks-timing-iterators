51fcd0701e7b91e9c5b74d910326e35b6b24ee3c
Ariel <Ariel@Maddies-MacBook-Pro.local>
2020-10-22 10:14:34 -0400

    [Hello] 10 repetitions done

---
+++ check.js
@@ -0,0 +1,16 @@
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+    return 'negative';
+} else {
+    return 'zero';
+}
+}
+
+console.log(checkNumber(5));
+
+
+
+function checkNumber
+
--- hello.js
+++ hello.js
@@ -33,3 +33,39 @@ function sayHello(name) {
 }
 
 console.log(sayHello('NYC'))
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+
+console.log(sayHello(''))
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '! Dont stop now.';
+    return msg;
+}
+
+console.log(sayHello('Almost Done'))
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you';
+    return msg;
+}
+
+console.log(sayHello('Even Closer'))
+
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+
+console.log (sayHello("Once More"))
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+
+console.log(sayHello("Done!"))
